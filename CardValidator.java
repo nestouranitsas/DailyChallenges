99dc09a24a9ab4caa2cb62cc60bf123990fa0bba
nestouranitsas <nestouranitsas@gmail.com>
2019-02-23 16:26:00 -0500

    fixed luhn algorithm
    was previously using wrong algorithm

--- CardValidator.java
+++ CardValidator.java
@@ -6,78 +6,34 @@ import java.util.Arrays;
 
 class CardValidator
 {
-    public Boolean validateCard(String cardNum)
+    public Boolean validateCcNum(String cardNum)
     {       
-        //get the last digit as checksum as int
-        String checkSumStr = cardNum.substring(cardNum.length()-1, cardNum.length());
-   
-        //Convert the checksum str to int
-        int checkSumInt = Integer.valueOf(checkSumStr);
-
-        //reverses the string and drops the first digit (previously last digit)
-        char[] split = cardNum.toCharArray();
-        String reverse = "";
-        for (int i = 2; i <= split.length; ++i)
-        {
-            reverse += split[split.length-i];
-        }
-
-        //convert reverse string into char array
-        char[] revChar = reverse.toCharArray();
-
-        //convert char array into int array
-        int[] revInt = new int[revChar.length];
-        for (int j = 0; j < revChar.length; j++)
-        {
-            revInt[j] = (int)revChar[j] - '0';
-        }
-
-        //doubles ODD digits
-        for (int i = 0; i < revInt.length; i++)
-        {
-            if (i%2 == 0)
-            {
-                revInt[i] = revInt[i]*2;
-            }
-        }
-
-        //subtracts 9 from all digts greater than 9
-        for (int i = 0; i < revInt.length; i++)
-        {
-            if (revInt[i] > 9)
-            {
-                revInt[i] = revInt[i] - 9;
-            }
-        }
-
-        //adds all numbers in revInt array
+        int nDigits = cardNum.length();   
+        
         int sum = 0;
-        for (int i = 0; i < revInt.length-1; ++i)
+        Boolean isSecond = false;
+        for (int i = nDigits-1; i>= 0; i--)
         {
-            sum += i;
-        }
-        System.out.println(sum);
+            int digit = cardNum.charAt(i) - '0';
+            
+            if (isSecond == true)
+            {
+                digit = digit*2;
+                sum += digit / 10;
+                sum += digit % 10;
 
-        Boolean isValid;
-        //checks modulo 10 of sum against checksum if equal isvalid is true, else fasle
-        if(sum%10 == checkSumInt)
-        {
-            isValid = true;
-        }
-        else 
-        {
-            isValid = false;
-        }
-        System.out.println(isValid);
-        return isValid;
+                isSecond = !isSecond;
+            }
+        }       
+        return (sum % 10 == 0);
     }
 
     public static void main (String[] args)
     {
         CardValidator myValidator = new CardValidator();
-        String cardNum = "4024007163401591727";
-        myValidator.validateCard(cardNum);
-        if (myValidator.validateCard(cardNum) == true)
+        String cardNum = "3541651733821524947";
+        myValidator.validateCcNum(cardNum);
+        if (myValidator.validateCcNum(cardNum) == true)
         {
             System.out.println("Card Number: " + cardNum + " is a VALID credit card number.");
         }
