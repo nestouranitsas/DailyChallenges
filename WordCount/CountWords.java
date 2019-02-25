eb914856d267fc727cb23f5681f9fd82a0256e8f
nestouranitsas <nestouranitsas@gmail.com>
2019-02-24 23:42:44 -0500

    created folder for word counter and input file.
    started work on word counter pushing to work mobily

---
+++ WordCount/CountWords.java
@@ -0,0 +1,16 @@
+public class CountWords
+{
+    public void wordCount()
+    {
+        String filename = "C://Documents/Java Projects/DailyChallenges/WordCount/wc_input.txt";
+        File file = new File(filename);
+        FileInputStream = fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, cs);
+        BufferedReader br = new BufferedReader(isr);
+
+    }
+    public static void main (String[] args)
+    {
+
+    }
+}
