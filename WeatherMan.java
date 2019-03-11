0b377cd1f4396c800d9107219b6303eb2cedf7b9
nestouranitsas <nestouranitsas@gmail.com>
2019-03-10 21:29:16 -0400

    Started WeatherMan project
    weatherman takes the user's zipcode/location and reports the weather back. updated spreadsheet to match

---
+++ WeatherMan.java
@@ -0,0 +1,19 @@
+//created on 3/10/2019 by Nestor Ouranitsas
+/*This program takes the user's State and zip code and fetches the
+local weather information using an weather API*/
+
+public class WeatherMan
+{
+    public void fetchWeather(String zipcode)
+    {
+
+    }
+    public void reportWeather()
+    {
+
+    }
+    public static void main (String[] args)
+{
+    WeatherMan wm = new WeatherMan();
+}
+}
