ca74a3a9d7e3ea87c5e910a0b7efb43d134b74da
nestourantisas <nestouranitsas@gmail.com>
2019-03-30 14:28:45 -0400

    created API library
    created API library changed structure of Weatherman

---
+++ WeatherMan/WeatherMan.java
@@ -0,0 +1,31 @@
+//created on 3/10/2019 by Nestor Ouranitsas
+/*This program takes the user's zip code and gets
+local weather information using an the dark sky API*/
+
+public class WeatherMan
+{
+    //gets GPS coordinates using google maps API
+    public void getCoordinates(int zipCode, String state)
+    {
+
+    }
+
+    //gets weather conditions from GPS coordinates using Dark Sky API
+    public void getWeather()
+    {
+
+    }
+
+    //reports weather in a readable format from dark sky API
+    public void reportWeather()
+    {
+
+    }
+    public static void main (String[] args)
+{
+    WeatherMan wm = new WeatherMan();
+    wm.getCoordinates();
+    wm.getWeather();
+    wm.reportWeather();
+}
+}
